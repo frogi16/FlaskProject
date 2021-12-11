ade5c48d0fc4c8602cbf5398e73659c4e6db91cc
Przemyslaw Skrobot <44038381+frogi16@users.noreply.github.com`>
2021-12-11 14:01:10 +0100

    Use Heroku port number

--- app.py
+++ app.py
@@ -49,11 +49,8 @@ def add_object():
 
 if __name__ == '__main__':
     import os
-    HOST = os.environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(os.environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
+    HOST = '0.0.0.0'
+    PORT = os.environ.get('PORT', 17995)
     SECRET_KEY = os.urandom(32)
     app.config['SECRET_KEY'] = SECRET_KEY
     app.run(HOST, PORT)
